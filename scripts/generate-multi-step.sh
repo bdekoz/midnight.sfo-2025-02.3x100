ab1bce56bbee44e8b8049448901721715967a90b
Benjamin De Kosnik <bdekosnik@mozilla.com>
2025-06-28 23:54:17 -0700

    filmstrip via control points

--- scripts/generate-multi-step.sh
+++ scripts/generate-multi-step.sh
@@ -15,8 +15,8 @@ SCRIPTSDIR=/home/bkoz/src/midnight.sfo/scripts/
 
 start_time=`date`
 $SCRIPTSDIR/generate-video-side-by-side-wrapper.sh $TDATE $TPMETADATA $TRDIR
-#$SCRIPTSDIR/generate-video-filmstrip-wrapper.sh $TDATE $TPMETADATA
-#$SCRIPTSDIR/generate-aggregate-json.sh $TDATE $TPMETADATA
+$SCRIPTSDIR/generate-video-filmstrip-wrapper.sh $TDATE $TPMETADATA $TRDIR
+$SCRIPTSDIR/generate-aggregate-json.sh $TDATE $TPMETADATA
 end_time=`date`
 
 echo "done"
