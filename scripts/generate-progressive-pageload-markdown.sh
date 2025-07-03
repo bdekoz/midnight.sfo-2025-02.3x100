b90f8732b92e5f3249b7a48be6f0afae2e8d44e3
Benjamin De Kosnik <bkoz@gnu.org>
2025-07-03 14:59:44 -0700

    add markdown

---
+++ scripts/generate-progressive-pageload-markdown.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+PAGELOADGLYPH=$MOZPERFAX/bin/moz-perf-x-analyze-progressive-pageload.exe
+
+for FILE in `ls *aggregate.json`; do
+    if [ -f "$FILE" ]; then
+	${PAGELOADGLYPH} $FILE
+    else
+	echo "$FILE" >> progressive-pageload.missing
+    fi
+done
+
+mv *.svg ../../resources/
+mv *.md ../../pages/
